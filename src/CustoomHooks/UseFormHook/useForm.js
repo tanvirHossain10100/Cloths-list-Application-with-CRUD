09cf5547e13d7ab4379065461f2a992528684f2e
tanvirHossain10100 <tanvirhossan528@gmail.com>
2023-11-30 00:45:42 +0600

    [DONE] max quantity condition done for products

--- src/CustoomHooks/UseFormHook/useForm.js
+++ src/CustoomHooks/UseFormHook/useForm.js
@@ -8,6 +8,11 @@ const useForm = (initialData) => {
   // }
   const handleChanges = (e) => {
     const { name, value } = e.target;
+    if (name === "quantity") {
+      if (value > 10) {
+        return alert("You can not add more then 10 products");
+      }
+    }
     console.log(value, "value");
     if (value < 0) {
       return alert("You can not put negative number");
